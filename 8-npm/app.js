ac618fae7336710a96afec632c9a3ecc77afdffa
randi089 <randifebriadi@gmail.com>
2024-07-09 16:56:49 +0700

    npm

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const validator = require("validator");
+const chalk = require("chalk");
+
+// console.log(validator.isEmail("randi@gmail.c"));
+// console.log(validator.isMobilePhone("0722345678", "id-ID"));
+// console.log(validator.isNumeric("0722345678a"));
+
+// console.log(chalk.italic.bgBlue.black("Hello World!"));
+const nama = "Randi";
+const pesan = chalk`Lorem ipsum dolor, {bgGreen.black.bold sit amet} consectetur {bgGreen.italic.black adipisicing} elit. At, blanditiis. Nama saya : ${nama}`;
+console.log(pesan);
