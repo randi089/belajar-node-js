46518bdc60b3fbc4158f153254be8baf6296ae3b
randi089 <randifebriadi@gmail.com>
2024-07-10 15:19:53 +0700

    contoh2

--- 12 Web Server/app.js
+++ 12 Web Server/app.js
@@ -1,7 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {});
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+    const url = req.url;
+    if (url === "/about") {
+      //   res.write("<h1>Ini Adalah Halaman About</h1>");
+      fs.readFile("./about.html", (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("File not found!");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+    } else if (url === "/contact") {
+      res.write("<h1>Ini Adalah Halaman Contact</h1>");
+      res.end();
+    } else {
+      //   res.write("Hello World!");
+      fs.readFile("./index.html", (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("File not found!");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+    }
+  })
+  .listen(3000, () => {
+    console.log("Server is listening on port 3000..");
+  });
