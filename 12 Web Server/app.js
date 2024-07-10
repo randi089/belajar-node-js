4d1844b478b574a6ed9d339e69f5629e1aa548a8
randi089 <randifebriadi@gmail.com>
2024-07-10 15:30:53 +0700

    menggunakan switch

--- 12 Web Server/app.js
+++ 12 Web Server/app.js
@@ -20,12 +20,16 @@ http
     });
 
     const url = req.url;
-    if (url === "/about") {
-      renderHTML("./about.html", res);
-    } else if (url === "/contact") {
-      renderHTML("./contact.html", res);
-    } else {
-      renderHTML("./index.html", res);
+    switch (url) {
+      case "/about":
+        renderHTML("./about.html", res);
+        break;
+      case "/contact":
+        renderHTML("./contact.html", res);
+        break;
+      default:
+        renderHTML("./index.html", res);
+        break;
     }
   })
   .listen(3000, () => {
