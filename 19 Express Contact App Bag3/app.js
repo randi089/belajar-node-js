f7d58d786beaacfd80b48aba2c7ceef4fb256dd0
randi089 <randifebriadi@gmail.com>
2024-07-11 15:40:36 +0700

    menambahkan fitur hapus

---
+++ 19 Express Contact App Bag3/app.js
@@ -0,0 +1,149 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const {
+  loadContact,
+  findContact,
+  addContact,
+  cekDuplikat,
+  deleteContact,
+} = require("./utils/contact");
+const { body, validationResult, check } = require("express-validator");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+
+const app = express();
+const port = 3000;
+
+// gunakan ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts); // Third party middleware
+app.use(express.static("public")); // Built-in middleware
+app.use(express.urlencoded({ extended: true })); // Built-in
+
+// konfigurasi flash
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+app.get("/", (req, res) => {
+  const mahasiswa = [
+    {
+      nama: "Randi Febriadi",
+      email: "randifebriadi@gmail.com",
+    },
+    {
+      nama: "Tasya Aryati Sakinah",
+      email: "tasyaaryati@gmail.com",
+    },
+    {
+      nama: "Rumzi Rahman",
+      email: "rumzirahman@gmail.com",
+    },
+  ];
+  res.render("index", {
+    layout: "layouts/main-layout",
+    nama: "Randi Febriadi",
+    title: "Halaman Home",
+    mahasiswa,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "Halaman About",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Halaman Contact",
+    contacts: loadContact(),
+    msg: req.flash("msg"),
+  });
+});
+
+// halaman tambah contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    layout: "layouts/main-layout",
+    title: "Form Tambah Contact",
+  });
+});
+
+// proses data contact
+app.post(
+  "/contact",
+  [
+    body("nama").custom((value) => {
+      const duplikat = cekDuplikat(value);
+      if (duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email tidak valid!").isEmail(),
+    check("nohp", "No HP tidak valid!").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render("add-contact", {
+        title: "Form Tambah Contact",
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+      });
+    } else {
+      addContact(req.body);
+
+      // kirimkan flash message
+      req.flash("msg", "Contact berhasil ditambahkan!");
+      res.redirect("/contact");
+    }
+  }
+);
+
+// proses delete contact
+app.get("/contact/delete/:nama", (req, res) => {
+  const contact = findContact(req.params.nama);
+
+  // jika contact tidak ada
+  if (!contact) {
+    res.status(404);
+    res.send("<h1>404</h1>");
+  } else {
+    deleteContact(req.params.nama);
+
+    // kirimkan flash message
+    req.flash("msg", "Contact berhasil dihapus!");
+    res.redirect("/contact");
+  }
+});
+
+// halaman detail contact
+app.get("/contact/:nama", (req, res) => {
+  res.render("detail", {
+    layout: "layouts/main-layout",
+    title: "Halaman Detail Contact",
+    contact: findContact(req.params.nama),
+  });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
